e85cc00fdfbbe2a48a7f30f757f42897a3a12dfc
plonsdale1 <petex.1995@outlook.com>
2019-08-29 21:25:45 +0000

    cats part two

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,22 @@
 const request = require('request');
 
-const url = process.argv[2];
+
+const breedName = process.argv[2].slice(4);
+const url = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
 
 
-
-
-request(url, function(error, response, body) {
-  const data = JSON.parse(body);
-  // console.log(data);
-  if (error) {
-    console.log("error at location!");
-  } else {
-    console.log("this is the data", data);
-    console.log(data[0].description);
-  }
-});
-
+const fetchBreedDescription = function(breedName, callback) {
+  request(url, function(error, name, body) {
+    const data = JSON.parse(body);
+    // console.log(data);
+    if (error) {
+      callback();
+      console.log("error at location!");
+    } else {
+      console.log("this is the data", data);
+      console.log(data[0].description);
+    }
+  });
+};
+fetchBreedDescription();
+module.exports = { fetchBreedDescription };
